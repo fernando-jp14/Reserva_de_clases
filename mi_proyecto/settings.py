67bbe4bfbbe4e11b5134d4b564de4762d735270a
Fernando Julca <fernandojulcap@gmail.com>
2025-09-11 12:49:38 -0500

    creando endpoints para usuarios -JWT

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'academia',
     'usuarios',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+class UserRegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
