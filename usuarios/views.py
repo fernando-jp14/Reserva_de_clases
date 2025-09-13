0c9812cc9e693ae538b26f42f52193c4ae4934a5
Fernando Julca <fernandojulcap@gmail.com>
2025-09-12 19:40:51 -0500

    actualizando

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,8 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from rest_framework.permissions import AllowAny
 
 class UserRegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
